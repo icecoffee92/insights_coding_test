91a59431e29304a1eda23d6ebb608cacad1fb6c2
icecoffee92 <georgegordon101@gmail.com>
2021-03-06 10:50:34 +0000

    Ensure that bash is installed in the image

--- app/run-apache.sh
+++ app/run-apache.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
-echo "docker" | sudo -S sed -i "s/80/${PORT:-80}/g" /etc/apache2/ports.conf
+#!/bin/bash
+echo "docker" | sudo -S sed -i "s/80/${PORT:-80}/g" /etc/apache2/ports.conf docker-php-entrypoint
 
 echo "docker" | sudo -S sed -i "s/VirtualHost \*:80/VirtualHost \*:${PORT:-80}/g" /etc/apache2/sites-available/000-default.conf
 
