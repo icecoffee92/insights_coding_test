3bf2e51d88232e9daee4c0d6dc994253da101107
icecoffee92 <georgegordon101@gmail.com>
2021-03-07 12:41:25 +0000

    Added composer.json file and vendor folder

---
+++ app/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '8eff2d7051e8012ff7f826ef1d6a6504787f998f',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8eff2d7051e8012ff7f826ef1d6a6504787f998f',
+    ),
+  ),
+);
