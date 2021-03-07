8fda2505048f4fc0f244c7b6e4c53b5cd0870ad4
icecoffee92 <georgegordon101@gmail.com>
2021-03-07 13:03:46 +0000

    --amend

--- app/vendor/composer/autoload_real.php
+++ app/vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit3049525228588a72ee65515707230626
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit3049525228588a72ee65515707230626', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit3049525228588a72ee65515707230626', 'loadClassLoader'));
@@ -50,6 +52,24 @@ class ComposerAutoloaderInit3049525228588a72ee65515707230626
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit3049525228588a72ee65515707230626::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire3049525228588a72ee65515707230626($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire3049525228588a72ee65515707230626($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
